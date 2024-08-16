9cf3b3c0f21acd74279f66e37640bd705465096c
MSNiranjan29 <adeptmohitsingh@outlook.com>
2024-08-16 12:38:08 +0530

    Configurations

---
+++ src/app/api.service.ts
@@ -0,0 +1,22 @@
+// src/app/api.service.ts
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../environments/environment'; // Make sure this import path is correct
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  private apiUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) {}
+
+  createAccount(data: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/create-account`, data);
+  }
+
+  createAccountWithMob(data: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/create-account1`, data);
+  }
+}
